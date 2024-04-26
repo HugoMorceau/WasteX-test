84229013573b119cca90a8258bdb3f9f802d0778
Hugo Morceau <106115253+HugoMorceau@users.noreply.github.com>
2024-04-26 17:43:25 +0200

    cleaned default app + basic tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content : ["./index.html","./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FF6363',
+        'secondary': {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+    }
   },
   plugins: [],
 }
