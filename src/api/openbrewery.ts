74d6ab4b54d1e0f4e5d314492d4ab8cca5b13f2e
Hugo Morceau <106115253+HugoMorceau@users.noreply.github.com>
2024-04-27 08:51:48 +0200

    api pagination

--- src/api/openbrewery.ts
+++ src/api/openbrewery.ts
@@ -3,14 +3,26 @@ import axios from 'axios'
 const API_BASE_URL = 'https://api.openbrewerydb.org'
 
 export const fetchBreweries = async (query: string) => {
+  let page = 1
+  const perPage = 200
+  let hasMore = true
+  const breweries: any[] = []
   try {
-    const response = await axios.get(
-      `${API_BASE_URL}/v1/breweries?by_name=${encodeURIComponent(query)}`
-    )
-    // const response = await axios.get(API_BASE_URL)
-    console.log('query:', query)
-    console.log('Fetched breweries:', response.data)
-    return response.data
+    while (hasMore) {
+      console.log('fetching page', page)
+
+      const response = await axios.get(
+        `${API_BASE_URL}/v1/breweries?by_name=${encodeURIComponent(query)}&page=${page}&per_page=${perPage}`
+      )
+      console.log('searched name:', query, 'page', page, 'hasMore', hasMore)
+      console.log('Fetched breweries:', response.data)
+      hasMore = response.data.length === perPage
+      page++
+      console.log(response.data.length)
+
+      breweries.push(...response.data)
+    }
+    return breweries
   } catch (error) {
     console.error('Failed to fetch breweries:', error)
     return []
--- src/stores/brewery.ts
+++ src/stores/brewery.ts
@@ -1,9 +1,27 @@
 import { defineStore } from 'pinia'
 import { fetchBreweries } from '../api/openbrewery'
+interface Brewery {
+  id: string
+  name: string
+  brewery_type: string
+  address_1: string | null
+  address_2: string | null
+  address_3: string | null
+  city: string | null
+  state_province: string | null
+  postal_code: string | null
+  country: string | null
+  longitude: string | null
+  latitude: string | null
+  phone: string | null
+  website_url: string | null
+  state: string | null
+  street: string | null
+}
 
 export const useBreweryStore = defineStore('brewery', {
   state: () => ({
-    breweries: [],
+    breweries: [] as Brewery[],
     query: ''
   }),
   actions: {
