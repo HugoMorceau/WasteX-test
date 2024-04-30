d05a219d6c9dad199a733a655be7fb6040f56912
Hugo Morceau <106115253+HugoMorceau@users.noreply.github.com>
2024-04-30 11:18:07 +0200

    auto trigger load more

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,39 @@
 import BreweryList from '@/components/BreweryList.vue'
 import SearchBar from '@/components/SearchBar.vue'
 import { useBreweryStore } from '@/stores/brewery'
+import { onMounted, onUnmounted, ref } from 'vue'
 
 const store = useBreweryStore()
+const loadMoreButton = ref(null)
 const loadMore = () => {
   store.loadMore()
 }
+let observer
+onMounted(() => {
+  observer = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+      loadMore()
+    }
+  })
+
+  observer.observe(loadMoreButton.value)
+})
+
+onUnmounted(() => {
+  if (observer) {
+    observer.disconnect()
+  }
+})
 </script>
 
 <template>
   <SearchBar />
   <BreweryList :breweries="store.breweries" />
-  <button @click="loadMore" class="mb-10 mt-4 flex w-full justify-center text-primary-hover">
+  <button
+    ref="loadMoreButton"
+    @click="loadMore"
+    class="mb-10 mt-4 flex w-full justify-center text-primary-hover"
+  >
     {{ store.hasMore ? 'Load More . . .' : '' }}
   </button>
 </template>
